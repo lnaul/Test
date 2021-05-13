39705992050ce610f8dd84a1b974cd74dc314c1a
lnaul <naumovdimitriy@gmail.com>
2021-05-13 08:41:23 +0700

    Menu & Level

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float rotateSpeed = 2;
+    public float rotSpd = 2;
+    public float rotSpdZ = 2;
+
+    void Update()
+    {
+        transform.Rotate(rotSpd * Time.timeScale, rotateSpeed * Time.timeScale, rotSpdZ * Time.timeScale, Space.World);
+    }
+}
